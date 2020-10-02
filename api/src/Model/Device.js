2707c1f325f8e516f993ce2a6f644d3020ff2dff
Ognian Baruh <ognian@baruh.net>
2020-10-02 17:32:18 +0300

    [UPDATE] Refactor Device model

--- api/src/Model/Device.js
+++ api/src/Model/Device.js
@@ -4,22 +4,6 @@ class Device {
 		this.name = name;
 	}
 
-	// get name() {
-	// 	return this._name;
-	// }
-
-	// get isSick() {
-	// 	return this._isInfected;
-	// }
-
-	sick() {
-		this.isInfected = 1;
-	}
-
-	recover() {
-		this.isInfected = 0;
-	}
-
 }
 
 module.exports = {
