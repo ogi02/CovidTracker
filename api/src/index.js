893e8cdbc467b0ec1122490f9046142a8a32ff2f
Ognian Baruh <ognian@baruh.net>
2020-10-02 19:38:06 +0300

    [UPDATE] Updated main js file

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,5 @@
 const { createConnection } = require('typeorm');
 
-<<<<<<< HEAD
-createConnection().then(function () {
-    const d = require('./Routes/Report');
-    console.log(d.report())
-=======
 createConnection().then(() => {
 
 	const express = require('express');
@@ -16,12 +11,16 @@ createConnection().then(() => {
 	const report = require('./Routes/Report');
 	const checkForContact = require('./Routes/CheckForContact');
 
+	app.use('/', (req, res, next) => {
+		console.log(req);
+		next();
+	})
+
 	app.post('/registerDevice', registerDevice.register);
-	// app.post('/contact', contact.sth);
-	// app.post('/report', report.sth);
+	app.post('/reportContact', contact.contact);
+	app.post('/report', report.report);
 	// app.get('/checkForContact', checkForContact.sth);
 
-	registerDevice.register("d1", "sth");
+	app.listen(3000)
 
->>>>>>> 64875695a0d9f626685d8713e94ee67c28e09ee0
 }).catch(err => console.log(err));
