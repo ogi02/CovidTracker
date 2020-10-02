bfdaed67b64e4a39479fcb118f8fa2104c537d9c
Ognian Baruh <ognian@baruh.net>
2020-10-02 17:03:17 +0300

    [UPDATE] Import routes and call them

--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,21 @@
 const { createConnection } = require('typeorm');
 
-createConnection().then().catch(err => console.log(err));
+createConnection().then(() => {
+
+	const express = require('express');
+	const app = express();
+	module.exports = app;
+
+	const registerDevice = require('./Routes/RegisterDevice');
+	const contact = require('./Routes/Contact');
+	const report = require('./Routes/Report');
+	const checkForContact = require('./Routes/CheckForContact');
+
+	app.post('/registerDevice', registerDevice.register);
+	// app.post('/contact', contact.sth);
+	// app.post('/report', report.sth);
+	// app.get('/checkForContact', checkForContact.sth);
+
+	registerDevice.register("d1", "sth");
+
+}).catch(err => console.log(err));
