15058464221567032ef64f35da993f9b08e5509b
Samuil Georgiev <sami.georgiev@gmail.com>
2020-10-02 13:46:50 +0300

    [UPDATE] Implemented report infectedness func

--- client/app/src/main/java/cc/holdinga/covidtracker/MainActivity.java
+++ client/app/src/main/java/cc/holdinga/covidtracker/MainActivity.java
@@ -1,25 +1,59 @@
 package cc.holdinga.covidtracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import okhttp3.*;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String deviceName = getDeviceName();
+
+    private String getDeviceName() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return mBluetoothAdapter.getName();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button alertButton = findViewById(R.id.alertButton);
-        alertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View View) {
 
-            }
-        });
+        Button alertButton = findViewById(R.id.alertButton);
+        alertButton.setOnClickListener(view -> reportInfectedness());
+    }
+
+    private void reportInfectedness() {
+        OkHttpClient httpClient = new OkHttpClient();
+        Request request = buildReportInfectednessRequest();
+        httpClient.newCall(request).enqueue(new ReportInfectednessResponseHandler());
+    }
+
+    private Request buildReportInfectednessRequest() {
+        RequestBody requestBody = new FormBody.Builder()
+                .add("deviceName", deviceName)
+                .build();
+        return new Request.Builder()
+                .url("https://api.mocki.io/v1/993b1ec5")
+                .post(requestBody)
+                .build();
+    }
+
+    private static class ReportInfectednessResponseHandler implements Callback {
+        @Override
+        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+        }
+
+        @Override
+        public void onResponse(@NonNull Call call, @NonNull Response response) {
+
+        }
     }
 }
