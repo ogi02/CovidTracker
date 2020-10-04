91c0bb2d8e10f928191ade9da3d3208b39bcd953
Samuil Georgiev <sami.georgiev@gmail.com>
2020-10-05 00:34:53 +0300

    [UPDATE] finished sending location

--- client/app/src/main/java/cc/holdinga/covidtracker/models/ContactForReporting.java
+++ client/app/src/main/java/cc/holdinga/covidtracker/models/ContactForReporting.java
@@ -1,14 +1,16 @@
 package cc.holdinga.covidtracker.models;
 
 public class ContactForReporting {
-
     private final String deviceName1;
-
     private final String deviceName2;
+    private final double latitude;
+    private final double longitude;
 
-    public ContactForReporting(String deviceName1, String deviceName2) {
+    public ContactForReporting(String deviceName1, String deviceName2, double latitude, double longitude) {
         this.deviceName1 = deviceName1;
         this.deviceName2 = deviceName2;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getDeviceName1() {
@@ -17,5 +19,13 @@ public class ContactForReporting {
 
     public String getDeviceName2() {
         return deviceName2;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
     }
 }
