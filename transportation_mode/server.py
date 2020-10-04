e99507dba16cf567422d5374b9ffd25f5b8d12df
Victor <victor@gorchilov.com>
2020-10-05 00:16:07 +0300

    [UPDATE] replace linear network for xgboost

--- transportation_mode/server.py
+++ transportation_mode/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request
-from numpy import argmax
-from tensorflow.keras.models import load_model
+from numpy import argmax, bincount
+from xgboost import XGBClassifier
 
 from utils import decode_output, order_params, parse_json
 
 app = Flask(__name__)
-model = load_model('model')
+xgb = XGBClassifier().load_model('xgb.dat')
+
+
+@app.route('/', methods=['GET'])
+def greeting():
+    return 'Hello, try sending me parameters to predict mode of transportation'
 
 
 @app.route('/', methods=['POST'])
@@ -14,7 +19,7 @@ def index():
         request.get_json()))  # split and order parameters
 
     params = params.reshape(1, -1)
-    out = model.predict(params)  # pass to array and predict mode
+    prediction = xgb.predict(params)
 
     # humanize output and pass to json
-    return jsonify(transport=decode_output(argmax(out)))
+    return jsonify(transport=decode_output(prediction))
