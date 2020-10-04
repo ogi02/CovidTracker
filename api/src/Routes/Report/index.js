3b1105943beaa82eb8676ab3712170577d5428b3
toniyanchev <anton.t.y2002@gmail.com>
2020-10-04 12:52:34 +0300

    [UPDATE] add comments in report-infectedness

--- api/src/Routes/Report/index.js
+++ api/src/Routes/Report/index.js
@@ -8,21 +8,25 @@ const contactRepository = getRepository(Contact);
 const pendingNotificationRepository = getRepository(PendingNotification);
 
 const reportInfectedness = async (req, res) => {
+    //Get user by name and check if it exists
     const device = await deviceRepository.findOne({ name: req.body.deviceName });
     if (!device) {
         res.status(422).json();
         return;
     }
 
+    //Set user to infected
     await deviceRepository.update({ name: req.body.deviceName }, { isInfected: 1 });
 
+    //Get all contacts for the infected user
     const contacts = await contactRepository.find({
         where: [
             {device1: device.id },
             {device2: device.id }, 
         ]
     });
-
+    
+    //Get all contacted users for the infected user 
     let contactedIds = [];
     contacts.forEach( c => {
         if (!contactedIds.includes(c.device1.id)) {
@@ -37,9 +41,11 @@ const reportInfectedness = async (req, res) => {
         }
     });
 
+    //Add contacted users to pending notifications
     let pendingNotifications = [];
     contactedIds.forEach( id => pendingNotifications.push({ infected: device.id, contacted: id }));
 
+    //Insert the notification
     const currentPendingNotifications = await pendingNotificationRepository.find({ infected: device.id });
     if (currentPendingNotifications.length === 0) {
         await pendingNotificationRepository.manager.insert(PendingNotification, pendingNotifications);
